8ef41ab8601f07ce352dae791c0a68344aaea569
Nicholas Walton <cactar65@gmail.com>
2019-10-31 00:56:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,11 @@
 class Student
   
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+  
+  def raise_hand
+    
   end
   
 end
